5a8969fc8c48e1808f2418e8cf57b964d560d4a5
seth-skocelas <s.skocelas@gmail.com>
2018-04-04 21:10:26 -0500

    Added Player and Team Model views, no coding work done on them yet. Cleaned up Player Stats View

---
+++ AreTheyGoodNBA/Controller/PlayerModelVC.swift
@@ -0,0 +1,19 @@
+//
+//  PlayerModelVC.swift
+//  AreTheyGoodNBA
+//
+//  Created by Seth Skocelas on 4/4/18.
+//  Copyright © 2018 Seth Skocelas. All rights reserved.
+//
+
+import UIKit
+
+class PlayerModelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ AreTheyGoodNBA/Controller/TeamModelVC.swift
@@ -0,0 +1,20 @@
+//
+//  TeamModelVC.swift
+//  AreTheyGoodNBA
+//
+//  Created by Seth Skocelas on 4/4/18.
+//  Copyright © 2018 Seth Skocelas. All rights reserved.
+//
+
+import UIKit
+
+class TeamModelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
